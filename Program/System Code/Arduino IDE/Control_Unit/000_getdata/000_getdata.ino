d03283f51cbcc87d522b35ce4ca97328f4f9d8d3
Nakul S <95758559+NakulSK221B@users.noreply.github.com>
2022-11-25 23:35:46 +0530

    Update 11:35pm

--- Program/System Code/Arduino IDE/Control_Unit/000_getdata/000_getdata.ino
+++ Program/System Code/Arduino IDE/Control_Unit/000_getdata/000_getdata.ino
@@ -5,11 +5,14 @@
 WiFiClient wifiClient;
 
 #define ON_Board_LED 2
+#define Valve_pin 4
 
 const char* ssid = "G";             // --> wifi name
 const char* password = "12345678";  //--> wifi password
 
 const char *host = "https://domestic-irrigation.000webhostapp.com:80/";
+
+// uint8_t Valve_pin = D2;
 
 String inString = "";
 void setup() {
@@ -22,7 +25,7 @@ void setup() {
 
   pinMode(ON_Board_LED, OUTPUT);
   digitalWrite(ON_Board_LED, HIGH);
-  pinMode(D3, OUTPUT);
+  pinMode(Valve_pin, OUTPUT);
   pinMode(D0, OUTPUT);
   digitalWrite(D0, LOW);
   Serial.print("Connecting");
@@ -57,18 +60,30 @@ void loop() {
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   int httpCodeGet = http.POST(getData);
   String payloadGet = http.getString();
+  if(payloadGet.indexOf('0')==0)
+  {   
+    digitalWrite(Valve_pin, LOW); 
+    Serial.println("Valve OFF");
+  }
+  else if(payloadGet.indexOf('1')==0)
+  {
+    digitalWrite(Valve_pin, HIGH);
+    Serial.println("Valve ON");     
+  }
   Serial.print("Response Code : ");
   Serial.println(httpCodeGet);
   Serial.print("Returned data from Server : ");
   Serial.println(payloadGet);
-  if(payloadGet[0]=='0')
-  {   
-    digitalWrite(D3, LOW);  
-  }
-  else if(payloadGet[0]=='1')
-  {
-    digitalWrite(D3, HIGH);     
-  }
+  // if(payloadGet[0]=='0')
+  // {   
+  //   digitalWrite(Valve_pin, LOW); 
+  //   Serial.print("Valve OFF");
+  // }
+  // else if(payloadGet[0]=='1')
+  // {
+  //   digitalWrite(Valve_pin, HIGH);
+  //   Serial.print("Valve ON");     
+  // }
   Serial.println("----------------Closing Connection----------------");
   http.end();
   Serial.println();
